09610ae22adfb010ffb971b8b683c16c077795b2
Nathan Cox <nathan@flyingmonkey.co.nz>
2014-11-28 09:59:48 +1300

    No longer manually requiring CSS.php - let the autoloader handle it (https://github.com/nathancox/silverstripe-minify/issues/4#issuecomment-64504356)

--- code/MinifyRequirements.php
+++ code/MinifyRequirements.php
@@ -1,29 +1,27 @@
 <?php
 
 class Minify_Requirements_Backend extends Requirements_Backend {
-	
+
 	static $rewrite_uris = true;
-	
+
 	protected function minifyFile($filename, $content) {
+
 		// if we have a javascript file and jsmin is enabled, minify the content
 		$isJS = stripos($filename, '.js');
 		if($isJS && $this->combine_js_with_jsmin) {
-			require_once('thirdparty/jsmin/jsmin.php');
+			//require_once('thirdparty/jsmin/jsmin.php');
 
 			increase_time_limit_to();
 			$content = JSMin::minify($content);
 		} else if (stripos($filename, '.css')) {
-			
-			// stolen shamelessly from Tonyair http://www.silverstripe.org/general-questions/show/14206
-			require_once('../' . $this->getModulePath() . '/thirdparty/min/lib/Minify/CSS.php'); 
-			
+
 			increase_time_limit_to();
 			$minifyCSSConfig = array();
 
 			if (self::$rewrite_uris) {
 				$minifyCSSConfig['currentDir'] = Director::baseFolder() . '/' . dirname($filename);
 			}
-			
+
 			$content = Minify_CSS::minify($content, $minifyCSSConfig);
 		}
 
@@ -31,16 +29,4 @@ class Minify_Requirements_Backend extends Requirements_Backend {
 		return $content;
 	}
 
-
-	function getModulePath() {
-		$path = dirname(__DIR__);
-		$path = str_replace(BASE_PATH.DIRECTORY_SEPARATOR, '', $path);
-		
-		// for windows
-		$path = str_replace('\\', '/', $path);
-		
-		return $path;
-	}
-
-	
 }
